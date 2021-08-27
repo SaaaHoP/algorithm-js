512099602a3f3082ce23e8729fa36384a1e34293
Park Sung Ho <qkrtjdgh97@gmail.com>
2021-08-27 14:58:06 +0900

    21.08.27/54
    21.08.27 programmers 54

---
+++ programmers/level2/2021.08.27/54-jadenCase.js
@@ -0,0 +1,28 @@
+function solution(s) {
+  let answer = '';
+  for (let i = 0; i < s.length; i++) {
+    console.log(s[i - 1]);
+    if (s[i] === ' ') {
+      answer += ' ';
+      continue;
+    } else {
+      if (s[i - 1] === ' ' || s[i - 1] === undefined) {
+        answer += s[i].toUpperCase();
+      } else {
+        answer += s[i].toLowerCase();
+      }
+    }
+  }
+  return answer;
+}
+
+function solution2(s) {
+  return s
+    .split(' ')
+    .map((v) => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase())
+    .join(' ');
+}
+
+console.log(solution2('3people   unFollowed me'));
+
+console.log('3people   unFollowed me'.split(' ').join(' '));
