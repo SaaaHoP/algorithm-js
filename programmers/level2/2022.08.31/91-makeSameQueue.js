185c82989be6063b7c5671bd5034c21730b174a8
Sung Ho Park <qkrtjdgh97@gmail.com>
2022-08-31 21:58:44 +0900

    22.08.31/91

---
+++ programmers/level2/2022.08.31/91-makeSameQueue.js
@@ -0,0 +1,30 @@
+function solution(queue1, queue2) {
+  const getSum = (arr) => arr.reduce((a, c) => a + c, 0);
+
+  if (getSum([...queue1, ...queue2]) % 2 !== 0) return -1;
+
+  const queue = [...queue1, ...queue2, ...queue1, ...queue2];
+  const maxCount = queue.length;
+  const average = getSum(queue) / 4;
+  let [currentSum, count, start, end] = [getSum(queue1), 0, 0, queue1.length];
+
+  const pop = () => {
+    currentSum -= queue[start];
+    start += 1;
+    count += 1;
+  };
+  const insert = () => {
+    currentSum += queue[end];
+    end += 1;
+    count += 1;
+  };
+
+  while (count <= maxCount) {
+    if (currentSum < average) insert();
+    if (currentSum > average) pop();
+    if (currentSum === average) return count;
+  }
+  return -1;
+}
+
+console.log(solution([3, 2, 7, 2], [4, 6, 5, 1]));
